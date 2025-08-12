37d2bb78a53b8a7d4e062d74359437415faf16e8
Alexander Albrecht <alexander.albrecht2004@gmail.com>
2025-08-12 10:01:34 +0200

    404-page with a little style ready

--- src/pages/404.php
+++ src/pages/404.php
@@ -1,1 +1,14 @@
 <?php
+require __DIR__ . '/../../partials/header.php';
+?>
+
+<div class="page-404">
+    <h1>404</h1>
+    <p>Ups! Die Seite konnte nicht gefunden werden.</p>
+    <a href="/" class="button">Zur Startseite</a>
+</div>
+</div>
+
+<?php
+require __DIR__ . '/../../partials/footer.php';
+ echo'</body></html>';
