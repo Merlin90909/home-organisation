9239c27296e8beb32c429cb4c52e01cd1d2bce3e
Alexander Albrecht <alexander.albrecht2004@gmail.com>
2025-11-11 09:00:13 +0100

    UserService full OrmService

--- framework/Services/UserService.php
+++ framework/Services/UserService.php
@@ -4,16 +4,12 @@ namespace Framework\Services;
 
 use App\Dtos\UserDto;
 use App\Entities\UserEntity;
-use Dom\Entity;
 use Framework\Interfaces\EntityInterface;
-use PDO;
-use Framework\Services\OrmService;
-
 
 class UserService
 {
 
-    public function __construct(private PDO $pdo, private OrmService $ormService)
+    public function __construct(private OrmService $ormService)
     {
     }
 
@@ -30,9 +26,10 @@ class UserService
 
     private function getUsers(): array
     {
-        $stmt = $this->pdo->query('SELECT * FROM user');
-        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+        $users = $this->ormService->findOneBy(
+            [],
+            UserEntity::class
+        );
         return $users;
     }
 
